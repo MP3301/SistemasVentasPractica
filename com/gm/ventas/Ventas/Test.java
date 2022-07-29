f97ac7db22bc9679ec4eb554072b973e6a5be9f2
mperez30 <67341832+mperez30@users.noreply.github.com>
2022-07-28 21:11:45 -0400

    1q

---
+++ com/gm/ventas/Ventas/Test.java
@@ -0,0 +1,17 @@
+package com.gm.ventas.Ventas;
+
+import com.gm.ventas.Orden;
+import com.gm.ventas.Producto;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa", 50.00);
+        Producto producto2 = new Producto("Pantalon", 100.00);
+
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+    }
+}
